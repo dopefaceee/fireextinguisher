c5300a51ca3fec0a2b67ff6dd855bb396e87c2e3
Ade Suluh N <ade.suluh@gmail.com>
2021-10-13 01:15:30 +0700

    initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="Hero container">
+    <div class="text-container">
+      <h1 class="header">Find your Fire Extinguisher</h1>
+      <p class="snippet">
+        Lorem ipsum dolor sit amet. Consectetur adiscpling elit. Iste lusto
+        quidem asperioce. Cum nisi elus ipsa antarte
+      </p>
+      <button class="button btn">Start Looking</button>
+    </div>
+    <img
+      class="image"
+      src="~assets/svg/fire-extinguisher.svg"
+      alt="fire extinguisher"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.Hero {
+  height: 50vh;
+  position: relative;
+  display: flex;
+  padding: 5rem 0;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+
+.text-container {
+  width: 50%;
+  margin-top: 4rem;
+}
+.header {
+  font-weight: 900;
+  font-size: 5rem;
+}
+.snippet {
+  color: grey;
+  font-size: 1.25rem;
+}
+.image {
+  width: 27.5rem;
+  margin-top: 2rem;
+}
+
+.button {
+  padding: 1rem 4rem;
+  border-radius: 100rem;
+  background-color: rgb(231, 81, 43);
+  color: white;
+  width: 20rem;
+  font-weight: 700;
+  transition: 0.5s;
+}
+.button:hover {
+  width: 21.5rem;
+}
+
+@media (max-width: 500px) {
+  .Hero {
+    height: 40vh;
+  }
+}
+</style>
